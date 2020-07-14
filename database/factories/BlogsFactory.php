03c84c2a98bbad9a2f6e21f8e35264e9925f392f
JuliaFakhr99 <maximova73julia.maximova@yandex.ru>
2020-07-14 22:09:15 +0300

    connbd

---
+++ database/factories/BlogsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\blogs;
+use Faker\Generator as Faker;
+
+$factory->define(blogs::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
